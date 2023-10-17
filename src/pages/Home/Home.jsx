c9c7fdc8c873a7d1ef4415e7e269cf50c2c4770d
Jubair <jubair.ahmed2838@gmail.com>
2023-10-17 13:34:02 +0600

    Add: user deleted functionality added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,34 +1,87 @@
-import { Link } from "react-router-dom"
-
+import { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
+import Swal from "sweetalert2";
 const Home = () => {
+  const loadedData = useLoaderData();
+  const [users, setUsers] = useState(loadedData);
+
+  const handleDelete = (_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/users/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+            const remaining = users.filter(user=> user._id !== _id)
+            setUsers(remaining)
+          });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        Swal.fire("Cancelled", "Your imaginary file is safe :)", "error");
+      }
+    });
+  };
   return (
     <>
-    <Link to="/register" className="btn btn-primary m-4">New User</Link>
-    <div className="overflow-x-auto">
-    <table className="table">
-      {/* head */}
-      <thead>
-        <tr>
-          <th></th>
-          <th>Name</th>
-          <th>Job</th>
-          <th>Favorite Color</th>
-        </tr>
-      </thead>
-      <tbody>
-        {/* row 1 */}
-        <tr className="hover">
-          <th>1</th>
-          <td>Cy Ganderton</td>
-          <td>Quality Control Specialist</td>
-          <td>Blue</td>
-        </tr>
-       
-      </tbody>
-    </table>
-  </div>
-    </>
-  )
-}
+      <Link to="/register" className="btn btn-primary m-4">
+        New User
+      </Link>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Gender</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id} className="hover">
+                <th></th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+                <td>{user.gender}</td>
 
-export default Home
+                <td className="flex items-center gap-4">
+                  <button onClick={() => handleDelete(user._id)}>
+                    <AiOutlineDelete
+                      className="text-primary cursor-pointer"
+                      size={20}
+                    />
+                  </button>
+                  <AiOutlineEdit
+                    className="text-primary cursor-pointer"
+                    size={20}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Home;
